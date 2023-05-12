49ad7b0df2ea7eb5e83321c3fda28e7a7f09fd0a
zyh <2729090745@qq.com>
2023-05-12 10:41:18 +0800

    second commit

--- src/main/java/com/example/gittest/GitTestApplication.java
+++ src/main/java/com/example/gittest/GitTestApplication.java
@@ -9,6 +9,7 @@ public class GitTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitTestApplication.class, args);
         System.out.println("hello world");
+        System.out.println("hello world1");
     }
 
 }
